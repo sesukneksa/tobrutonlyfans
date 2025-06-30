fe50aa3b457a65ec569c62d9378afc73c92f25c0
sesukneksa <sesukneksa@gmail.com>
2025-06-30 18:55:50 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cewektobrutcolmek.pages.dev',
+  site: 'https://tobrutonlyfans.pages.dev',
   output: 'server',
   adapter: cloudflare(),
   integrations: 
--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Tobrut Onlyfans';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Korea Tante China';
+export const desk = 'Video Bokep Indo Jepang Jav Hijab Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
 export const url = 'https://tobrutonlyfans.pages.dev';
 export const terbit = '2025-06-27T00:01:03Z';
